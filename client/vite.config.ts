b3011fb6e2089b78a02a25af9992852bec09afff
Michael Fleming <mikahl171@gmail.com>
2024-12-09 12:56:35 -0600

    pushing functioning app with client and server connection

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,10 +6,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001', // Backend server
-        changeOrigin: true, // Ensure the origin of the request matches the target
-        secure: false, // Ignore SSL verification if necessary
-        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: strip '/api' if your server routes do not include it
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
